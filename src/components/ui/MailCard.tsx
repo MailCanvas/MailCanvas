54bdb5d6447de55cb78394585db8bc852848ab3d
Taeju <79253468+taeju-moon@users.noreply.github.com>
2025-01-18 22:23:23 +0900

    [feat] Card에 링크 넣기

--- src/components/ui/MailCard.tsx
+++ src/components/ui/MailCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import mail from "../../../public/mail.png";
 import Chip from "./Chip";
+import Link from "next/link";
 
 interface MailCardProps {
   id: string;
@@ -10,34 +11,35 @@ interface MailCardProps {
 }
 
 const MailCard: React.FC<MailCardProps> = ({ id, title, tags, writer }) => {
-  console.log(id);
   return (
-    <div
-      className="w-[300px] h-[200px] bg-[#4caf50] p-3 rounded-lg flex flex-col justify-between text-primary
+    <Link href={`/detail/${id}`}>
+      <div
+        className="w-[300px] h-[200px] bg-[#4caf50] p-3 rounded-lg flex flex-col justify-between text-primary
       mt-5"
-      style={{
-        backgroundImage: `url(${mail.src})`,
-        display: "grid",
-        gridTemplateRows: "50px 1fr 30px",
-      }}
-    >
-      <h3 className="text-xl font-bold mb-50 " style={{ gridRow: 1 }}>
-        {title}
-      </h3>
-      <div
-        className="flex flex-row gap-2 justify-start flex-wrap overflow-x-auto scrollbar-hide"
-        style={{ gridRow: 2 }}
+        style={{
+          backgroundImage: `url(${mail.src})`,
+          display: "grid",
+          gridTemplateRows: "50px 1fr 30px",
+        }}
       >
-        {tags.map((tag, index) => (
-          <Chip tag={tag} key={index} />
-        ))}
-      </div>
+        <h3 className="text-xl font-bold mb-50 " style={{ gridRow: 1 }}>
+          {title}
+        </h3>
+        <div
+          className="flex flex-row gap-2 justify-start flex-wrap overflow-x-auto scrollbar-hide"
+          style={{ gridRow: 2 }}
+        >
+          {tags.map((tag, index) => (
+            <Chip tag={tag} key={index} />
+          ))}
+        </div>
 
-      <div
-        className="text-right text-xs mt-2 font-bold"
-        style={{ gridRow: 3 }}
-      >{`작성자: ${writer}`}</div>
-    </div>
+        <div
+          className="text-right text-xs mt-2 font-bold"
+          style={{ gridRow: 3 }}
+        >{`작성자: ${writer}`}</div>
+      </div>
+    </Link>
   );
 };
 
