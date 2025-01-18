1f3de7294c308649f6a5947474e3f2463f005769
xeonsim <xeonsim@gmail.com>
2025-01-18 19:44:22 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useGetForms } from "@/hooks/queries";
+import MailCard from "@/components/ui/MailCard";
 export default function Home() {
   // const title, tags, sortByCopyCount
   const [title, setTitle] = useState<string>("");
@@ -17,11 +18,13 @@ export default function Home() {
       {data.pages.map((page, i) => (
         <div key={i}>
           {page.forms.map((form, idx) => (
-            <div key={idx} className="border p-4 mb-4 rounded-lg">
-              <h2 className="text-xl font-bold">{form.Title}</h2>
-              <p>{form.Content}</p>
-              <span className="text-gray-500">{form.CopiedCount}</span>
-            </div>
+            <MailCard
+              key={idx}
+              id={form.id}
+              title={form.Title}
+              tags={form.tags}
+              writer={form.Writer}
+            />
           ))}
         </div>
       ))}
