f18bf946d70829d0b8e99d809c80a7d25023f4f8
문태주/Taeju <79253468+taeju-moon@users.noreply.github.com>
2025-01-19 16:31:00 +0900

    feature/Nav바-만들기 (#9)

---
+++ src/components/ui/Navigation/ClosedNavigation.tsx
@@ -0,0 +1,38 @@
+interface ClosedNavigationProps {
+  setIsNavBarOpen: (a: boolean) => void;
+}
+
+const ClosedNavigation: React.FC<ClosedNavigationProps> = ({
+  setIsNavBarOpen,
+}) => {
+  return (
+    <button
+      type="button"
+      className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none 
+      focus:ring-green-300 font-medium rounded-full text-sm p-2.5 text-center ml-2
+      justify-center me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800
+      h-[35px] mt-1
+      "
+      onClick={() => setIsNavBarOpen(true)}
+    >
+      <svg
+        className="w-4 h-4"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 14 10"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M1 5h12m0 0l-4 4m4-4l-4-4" // 화살표 방향 변경
+        />
+      </svg>
+      <span className="sr-only">Icon description</span>
+    </button>
+  );
+};
+
+export default ClosedNavigation;
