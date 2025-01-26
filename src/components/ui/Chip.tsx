62a8e6b5f523378904277fd8fd1a7d34e6283361
문태주/Taeju <79253468+taeju-moon@users.noreply.github.com>
2025-01-26 16:40:49 +0900

    Feature/검색창 아래에 칩 필터 버튼 추가 (#20)
    * [feat] Chip 버튼 디자인 시스템
    
    * [feat] 태그 버튼 기능 구현

--- src/components/ui/Chip.tsx
+++ src/components/ui/Chip.tsx
@@ -1,17 +1,36 @@
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 interface ChipProps {
   tag: string;
+  useLink: boolean;
 }
 
-const Chip: React.FC<ChipProps> = ({ tag }) => {
-  return (
-    <span
-      className="flex bg-white text-[#4caf50] border-2 border-[#4caf50] rounded-full px-3 py-1
-  text-sm hover:bg-[#6fbf73] hover:text-white transition-colors duration-300 h-[25px] items-center justify-center"
-    >
+const Chip: React.FC<ChipProps> = ({ tag, useLink }) => {
+  const chipContent = (
+    <span className="inline-flex items-center justify-center px-3 py-1 rounded-full text-sm bg-green-100 text-green-800 h-[25px] leading-[25px]">
       {tag}
     </span>
+  );
+
+  const handleClick = () => {
+    // 페이지를 강제로 리로드하면서 쿼리 파라미터 변경
+    window.location.href = `/?tag=${tag}`;
+  };
+
+  return (
+    <>
+      {useLink ? (
+        // useLink가 true일 때 링크 클릭 시 강제로 리로드
+        <button onClick={handleClick} className="text-green-800">
+          {chipContent}
+        </button>
+      ) : (
+        // useLink가 false일 때는 span만 렌더링
+        chipContent
+      )}
+    </>
   );
 };
 
