9448ed4a2e25762b1e4339c74b6505062d79ff93
Taeju <79253468+taeju-moon@users.noreply.github.com>
2025-02-01 14:20:49 +0900

    version-info-setting

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,11 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
 import MAIL_IMG from "../../../public/mail.png";
+
+import fs from "fs";
+import path from "path";
+
+export function getVersion() {
+  const packageJsonPath = path.join(process.cwd(), "package.json");
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+  return packageJson.version;
+}
+
 export default function Header() {
   return (
     <div className="w-full p-4 shadow text-4xl bg-gradient-to-t from-green-300 to-purple-300 text-center text-slate-100 font-extrabold">
       <a href="/" className="flex justify-center gap-3">
         <Image src={MAIL_IMG} alt="logo" height={30} /> MailCanvas
+        <p className="text-sm">ver {getVersion()}</p>
       </a>
     </div>
   );
