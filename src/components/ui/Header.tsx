37bdc13cb0b423a95b11636b60292c374302ddc2
Taeju <79253468+taeju-moon@users.noreply.github.com>
2025-02-15 16:31:38 +0900

    [feat] 링크 넣기

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -14,10 +14,13 @@ export function getVersion() {
 export default function Header() {
   return (
     <div className="w-full p-4 shadow text-4xl bg-gradient-to-t from-green-300 to-purple-300 text-center text-slate-100 font-extrabold">
-      <a href="/" className="flex justify-center gap-3">
-        <Image src={MAIL_IMG} alt="logo" height={30} /> MailCanvas
-        <p className="text-sm">ver {getVersion()}</p>
-      </a>
+      <div className="flex justify-center gap-3">
+        <Image src={MAIL_IMG} alt="logo" height={30} />
+        <a href="/">MailCanvas</a>
+        <a href="/patch-notes" className="text-sm">
+          ver {getVersion()}
+        </a>
+      </div>
     </div>
   );
 }
